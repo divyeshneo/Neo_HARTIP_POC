4045b767f205dbc4f5f9029102ac19202ada76b4
divyesh patel <58161045+divyeshpatelca@users.noreply.github.com>
2021-10-19 18:33:18 +0530

    First commit for POC
    First commit for POC

---
+++ EasyModbusClientExample/MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: srossmann
+ * Date: 13.02.2015
+ * Time: 11:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace EasyModbusClientExample
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button btnReadHoldingRegisters;
+		private System.Windows.Forms.TextBox txtStartingAddressInput;
+		private System.Windows.Forms.Label txtStartingAddress;
+		private System.Windows.Forms.Label txtNumberOfValues;
+		private System.Windows.Forms.TextBox txtNumberOfValuesInput;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnReadHoldingRegisters = new System.Windows.Forms.Button();
+            this.txtStartingAddressInput = new System.Windows.Forms.TextBox();
+            this.txtStartingAddress = new System.Windows.Forms.Label();
+            this.txtNumberOfValues = new System.Windows.Forms.Label();
+            this.txtNumberOfValuesInput = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblStopbits = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnReadHoldingRegisters
+            // 
+            this.btnReadHoldingRegisters.Location = new System.Drawing.Point(466, 32);
+            this.btnReadHoldingRegisters.Name = "btnReadHoldingRegisters";
+            this.btnReadHoldingRegisters.Size = new System.Drawing.Size(99, 36);
+            this.btnReadHoldingRegisters.TabIndex = 7;
+            this.btnReadHoldingRegisters.Text = "Send";
+            this.btnReadHoldingRegisters.UseVisualStyleBackColor = true;
+            this.btnReadHoldingRegisters.Click += new System.EventHandler(this.btnReadHoldingRegisters_Click);
+            // 
+            // txtStartingAddressInput
+            // 
+            this.txtStartingAddressInput.Location = new System.Drawing.Point(179, 32);
+            this.txtStartingAddressInput.Multiline = true;
+            this.txtStartingAddressInput.Name = "txtStartingAddressInput";
+            this.txtStartingAddressInput.Size = new System.Drawing.Size(281, 36);
+            this.txtStartingAddressInput.TabIndex = 9;
+            this.txtStartingAddressInput.Text = "1";
+            // 
+            // txtStartingAddress
+            // 
+            this.txtStartingAddress.Location = new System.Drawing.Point(38, 91);
+            this.txtStartingAddress.Name = "txtStartingAddress";
+            this.txtStartingAddress.Size = new System.Drawing.Size(118, 31);
+            this.txtStartingAddress.TabIndex = 10;
+            this.txtStartingAddress.Text = "Send Data";
+            this.txtStartingAddress.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtNumberOfValues
+            // 
+            this.txtNumberOfValues.Location = new System.Drawing.Point(4, 152);
+            this.txtNumberOfValues.Name = "txtNumberOfValues";
+            this.txtNumberOfValues.Size = new System.Drawing.Size(152, 28);
+            this.txtNumberOfValues.TabIndex = 12;
+            this.txtNumberOfValues.Text = "Receive Data/Response";
+            this.txtNumberOfValues.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.txtNumberOfValues.Click += new System.EventHandler(this.txtNumberOfValues_Click);
+            // 
+            // txtNumberOfValuesInput
+            // 
+            this.txtNumberOfValuesInput.Location = new System.Drawing.Point(179, 91);
+            this.txtNumberOfValuesInput.Multiline = true;
+            this.txtNumberOfValuesInput.Name = "txtNumberOfValuesInput";
+            this.txtNumberOfValuesInput.Size = new System.Drawing.Size(281, 36);
+            this.txtNumberOfValuesInput.TabIndex = 11;
+            this.txtNumberOfValuesInput.Text = "1";
+            this.txtNumberOfValuesInput.TextChanged += new System.EventHandler(this.txtNumberOfValuesInput_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 152);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(455, 157);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblStopbits
+            // 
+            this.lblStopbits.Location = new System.Drawing.Point(7, 32);
+            this.lblStopbits.Name = "lblStopbits";
+            this.lblStopbits.Size = new System.Drawing.Size(149, 34);
+            this.lblStopbits.TabIndex = 48;
+            this.lblStopbits.Text = "Command Send";
+            this.lblStopbits.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 316);
+            this.Controls.Add(this.lblStopbits);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNumberOfValues);
+            this.Controls.Add(this.txtNumberOfValuesInput);
+            this.Controls.Add(this.txtStartingAddress);
+            this.Controls.Add(this.txtStartingAddressInput);
+            this.Controls.Add(this.btnReadHoldingRegisters);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Neo Client";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblStopbits;
+    }
+}
---
+++ EasyModbusClientExample/MainForm.cs
@@ -0,0 +1,383 @@
+﻿/*
+Copyright (c) 2018-2020 Rossmann-Engineering
+Permission is hereby granted, free of charge, 
+to any person obtaining a copy of this software
+and associated documentation files (the "Software"),
+to deal in the Software without restriction, 
+including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit 
+persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission 
+notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EasyModbusClientExample
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private EasyModbus.ModbusClient modbusClient;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+            
+			modbusClient = new EasyModbus.ModbusClient("COM3");
+            modbusClient.ReceiveDataChanged += new EasyModbus.ModbusClient.ReceiveDataChangedHandler(UpdateReceiveData);
+            modbusClient.SendDataChanged += new EasyModbus.ModbusClient.SendDataChangedHandler(UpdateSendData);
+            modbusClient.ConnectedChanged += new EasyModbus.ModbusClient.ConnectedChangedHandler(UpdateConnectedChanged);
+            //          modbusClient.LogFileFilename = "logFiletxt.txt";
+
+            //modbusClient.Baudrate = 9600;
+            //modbusClient.UnitIdentifier = 2;
+
+        }
+
+        string receiveData = null;
+		
+		void UpdateReceiveData(object sender)
+		{
+            receiveData = "Rx: " + BitConverter.ToString(modbusClient.receiveData).Replace("-", " ") + System.Environment.NewLine;
+            Thread thread = new Thread(updateReceiveTextBox);
+            thread.Start();
+        }
+        delegate void UpdateReceiveDataCallback();
+        void updateReceiveTextBox()
+        {
+            if (textBox1.InvokeRequired)
+            {
+                UpdateReceiveDataCallback d = new UpdateReceiveDataCallback(updateReceiveTextBox);
+                this.Invoke(d, new object[] {  });
+            }
+            else
+            {
+                textBox1.AppendText(receiveData);
+            }
+        }
+
+        string sendData = null;
+        void UpdateSendData(object sender)
+		{
+            sendData = "Tx: " + BitConverter.ToString(modbusClient.sendData).Replace("-", " ") + System.Environment.NewLine;
+            Thread thread = new Thread(updateSendTextBox);
+            thread.Start();
+
+        }
+
+        void updateSendTextBox()
+        {
+            if (textBox1.InvokeRequired)
+            {
+                UpdateReceiveDataCallback d = new UpdateReceiveDataCallback(updateSendTextBox);
+                this.Invoke(d, new object[] { });
+            }
+            else
+            {
+                textBox1.AppendText(sendData);
+            }
+        }
+		
+		void BtnConnectClick(object sender, EventArgs e)
+		{
+			
+			modbusClient.Connect();
+		}
+		void BtnReadCoilsClick(object sender, EventArgs e)
+		{
+            try
+            {
+                if (!modbusClient.Connected)
+                {
+                    button3_Click(null, null);
+                }
+                bool[] serverResponse = modbusClient.ReadCoils(int.Parse(txtStartingAddressInput.Text)-1, int.Parse(txtNumberOfValuesInput.Text));
+               
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message,"Exception Reading values from Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReadDiscreteInputs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!modbusClient.Connected)
+                {
+                    button3_Click(null, null);
+                }
+                bool[] serverResponse = modbusClient.ReadDiscreteInputs(int.Parse(txtStartingAddressInput.Text)-1, int.Parse(txtNumberOfValuesInput.Text));
+               
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Exception Reading values from Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReadHoldingRegisters_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!modbusClient.Connected)
+                {
+                    button3_Click(null, null);
+                }
+
+
+               int[] serverResponse = modbusClient.ReadHoldingRegisters(int.Parse(txtStartingAddressInput.Text)-1, int.Parse(txtNumberOfValuesInput.Text));
+
+               
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Exception Reading values from Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReadInputRegisters_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!modbusClient.Connected)
+                {
+                    button3_Click(null, null);
+                }
+                
+                int[] serverResponse = modbusClient.ReadInputRegisters(int.Parse(txtStartingAddressInput.Text)-1, int.Parse(txtNumberOfValuesInput.Text));
+  
+               
+              
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Exception Reading values from Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbbSelctionModbus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (modbusClient.Connected)
+                modbusClient.Disconnect();
+        }
+
+        private void cbbSelectComPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (modbusClient.Connected)
+                modbusClient.Disconnect();
+           
+        }
+		
+		void TxtSlaveAddressInputTextChanged(object sender, EventArgs e)
+		{
+            try
+            {
+                
+            }
+            catch (FormatException)
+            { }	
+		}
+
+        bool listBoxPrepareCoils = false;
+        private void btnPrepareCoils_Click(object sender, EventArgs e)
+        {
+            
+            listBoxPrepareCoils = true;
+            listBoxPrepareRegisters = false;
+          
+
+        }
+        bool listBoxPrepareRegisters = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            listBoxPrepareRegisters = true;
+            listBoxPrepareCoils = false;
+        }
+
+        private void btnWriteSingleCoil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!modbusClient.Connected)
+                {
+                    button3_Click(null, null);
+                }
+
+                bool coilsToSend = false;
+
+               
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Exception writing values to Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnWriteSingleRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!modbusClient.Connected)
+                {
+                    button3_Click(null, null);
+                }
+
+                int registerToSend = 0;
+
+             
+
+               
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Exception writing values to Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnWriteMultipleCoils_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!modbusClient.Connected)
+                {
+                    button3_Click(null, null);
+                }
+
+              
+
+                
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Exception writing values to Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnWriteMultipleRegisters_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!modbusClient.Connected)
+                {
+                    button3_Click(null, null);
+                }
+
+               
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Exception writing values to Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lsbAnswerFromServer_DoubleClick(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        private void txtCoilValue_DoubleClick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRegisterValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (modbusClient.Connected)
+                    modbusClient.Disconnect();
+              
+                modbusClient.Connect();
+                
+              
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Unable to connect to Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateConnectedChanged(object sender)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            modbusClient.Disconnect();
+        }
+
+        private void txtBaudrate_TextChanged(object sender, EventArgs e)
+        {
+            if (modbusClient.Connected)
+                modbusClient.Disconnect();
+           
+
+          
+        }
+
+        private void txtNumberOfValues_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumberOfValuesInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
